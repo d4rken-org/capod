54e59adf01f5b36a991073054d5e37d857ade03d
darken <darken@darken.eu>
2022-01-04 21:06:33 +0100

    Improve tests

---
+++ app/src/test/java/eu/darken/capod/pods/core/apple/BasicSingleApplePodsTest.kt
@@ -0,0 +1,30 @@
+package eu.darken.capod.pods.core.apple
+
+import io.kotest.matchers.shouldBe
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.jupiter.api.Test
+
+class BasicSingleApplePodsTest : BaseAirPodsTest() {
+
+    @Test
+    fun `test mapping`() = runBlockingTest {
+        create<BasicSingleApplePods>("07 19 01 05 20 00 F5 0F 01 01 00 6D CE C0 04 22 0A 85 31 2D 82 6B 42 80 01 20 1A") {
+            rawPrefix shouldBe 0x01.toUByte()
+            rawDeviceModel shouldBe 0x0520.toUShort()
+            rawStatus shouldBe 0x00.toUByte()
+            rawPodsBattery shouldBe 0xF5.toUByte()
+            rawCaseBattery shouldBe 0x0F.toUByte()
+            rawCaseLidState shouldBe 0x01.toUByte()
+            rawDeviceColor shouldBe 0x01.toUByte()
+            rawSuffix shouldBe 0x00.toUByte()
+        }
+    }
+
+    @Test
+    fun `test battery headset percent`() = runBlockingTest {
+        create<BasicSingleApplePods>("07 19 01 05 20 00 F5 0F 01 01 00 6D CE C0 04 22 0A 85 31 2D 82 6B 42 80 01 20 1A") {
+            batteryHeadsetPercent shouldBe 0.5f
+        }
+    }
+
+}
---
+++ app/src/test/java/eu/darken/capod/pods/core/apple/SingleApplePodsTest.kt
@@ -0,0 +1,36 @@
+package eu.darken.capod.pods.core.apple
+
+import eu.darken.capod.pods.core.apple.airpods.AirPodsMax
+import io.kotest.matchers.shouldBe
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.jupiter.api.Test
+
+class SingleApplePodsTest : BaseAirPodsTest() {
+
+    @Test
+    fun `default bit mapping Max`() = runBlockingTest {
+        create<SingleApplePods>("07 19 01 0A 20 62 04 80 01 0F 40 0D 70 50 16 F2 40 83 16 BF 10 16 34 9B 74 84 E8") {
+            rawPrefix shouldBe 0x01.toUByte()
+            rawDeviceModel shouldBe 0x0A20.toUShort()
+            rawStatus shouldBe 0x62.toUByte()
+            rawPodsBattery shouldBe 0x04.toUByte()
+            rawCaseBattery shouldBe 0x80.toUByte()
+            rawCaseLidState shouldBe 0x01.toUByte()
+            rawDeviceColor shouldBe 0x0F.toUByte()
+            rawSuffix shouldBe 0x40.toUByte()
+        }
+    }
+
+    @Test
+    fun `test values based on AirPodMax`() = runBlockingTest {
+        create<AirPodsMax>("07 19 01 0A 20 02 05 80 04 0F 44 A7 60 9B F8 3C FD B1 D8 1C 61 EA 82 60 A3 2C 4E") {
+            batteryHeadsetPercent shouldBe 0.5f
+
+            isCaseCharging shouldBe false
+            isHeadsetBeingCharged shouldBe false
+
+            isHeadsetBeingWorn shouldBe true
+            batteryCasePercent shouldBe 0.0f
+        }
+    }
+}
