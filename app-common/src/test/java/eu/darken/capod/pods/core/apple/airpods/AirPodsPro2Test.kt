9ece40f26e9fbce241580017a0c7ae41e786a2b1
darken <darken@darken.eu>
2022-09-24 11:42:11 +0200

    Add second AirPods Pro 2 test case from reddit user.

--- app-common/src/test/java/eu/darken/capod/pods/core/apple/airpods/AirPodsPro2Test.kt
+++ app-common/src/test/java/eu/darken/capod/pods/core/apple/airpods/AirPodsPro2Test.kt
@@ -42,4 +42,39 @@ class AirPodsPro2Test : BaseAirPodsTest() {
             podStyle.identifier shouldBe HasAppleColor.DeviceColor.WHITE.name
         }
     }
+
+    /**
+     * https://old.reddit.com/message/messages/1hst12h
+     */
+    @Test
+    fun `test AirPods Pro 2 - unknown setup from reddit user`() = runBlockingTest {
+        create<AirPodsPro2>("07 19 01 14 20 2B 9A 8F 01 00 04 0F 26 1A C4 2B FA 2F B9 B6 08 CD 60 CB DF 75 AB") {
+
+            rawPrefix shouldBe 0x01.toUByte()
+            rawDeviceModel shouldBe 0x1420.toUShort()
+            rawStatus shouldBe 0x2B.toUByte()
+            rawPodsBattery shouldBe 0x9A.toUByte()
+            rawFlags shouldBe 0x8.toUShort()
+            rawCaseBattery shouldBe 0xF.toUShort()
+            rawCaseLidState shouldBe 0x01.toUByte()
+            rawDeviceColor shouldBe 0x0.toUByte()
+            rawSuffix shouldBe 0x04.toUByte()
+
+            isLeftPodMicrophone shouldBe true
+            isRightPodMicrophone shouldBe false
+
+            isLeftPodInEar shouldBe true
+            isRightPodInEar shouldBe true
+
+            batteryLeftPodPercent shouldBe 1.0f
+            batteryRightPodPercent shouldBe 0.9f
+
+            isCaseCharging shouldBe false
+            isRightPodCharging shouldBe false
+            isLeftPodCharging shouldBe false
+            batteryCasePercent shouldBe null
+
+            podStyle.identifier shouldBe HasAppleColor.DeviceColor.WHITE.name
+        }
+    }
 }
