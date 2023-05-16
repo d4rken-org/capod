50bccbaa2e7e476bb72469b53ef75572a1418c2a
darken <darken@darken.eu>
2023-05-16 09:14:06 +0200

    Update Crowdin tooling

--- crowdin.sh
+++ crowdin.sh
@@ -1,3 +1,4 @@
-export CROWDIN_API_KEY=`cat ~/.appconfig/eu.darken.capod/crowdin.key`
+#!/bin/sh
+export CROWDIN_PERSONAL_TOKEN=`cat ~/.appconfig/eu.darken.capod/crowdin.key`
 alias crowdin-cli='java -jar crowdin-cli.jar'
 crowdin-cli "$@"
