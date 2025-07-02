6b2073eff2cf9b5d4afddba73a3e8dd8f809eb6f
darken <darken@darken.eu>
2025-07-02 16:52:07 +0200

    Update AGP, SDK, and BillingClient
    This commit updates:
    - Android Gradle Plugin to 8.11.0
    - Compile/Target SDK to 36
    - Google Play Billing Library to 8.0.0
    
    The `BillingClient` is updated to use the new `PendingPurchasesParams` and `QueryPurchasesParams` APIs.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -8,7 +8,7 @@ repositories {
     mavenCentral()
 }
 dependencies {
-    implementation("com.android.tools.build:gradle:8.10.1")
+    implementation("com.android.tools.build:gradle:8.11.0")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.21")
     implementation("com.squareup:javapoet:1.13.0")
 }
