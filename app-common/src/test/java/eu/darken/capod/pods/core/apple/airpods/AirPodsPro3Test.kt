15054d836d8468095729df17d557ef90e7a61b8e
darken <darken@darken.eu>
2025-09-21 18:38:13 +0200

    Merge branch 'airpods-pro-3' into airpods-pro-3-merged
    # Conflicts:
    #	app-common/src/main/java/eu/darken/capod/pods/core/PodDevice.kt
    #	app-common/src/main/java/eu/darken/capod/pods/core/apple/airpods/AirPodsPro3.kt

--- app-common/src/test/java/eu/darken/capod/pods/core/apple/airpods/AirPodsPro3Test.kt
+++ app-common/src/test/java/eu/darken/capod/pods/core/apple/airpods/AirPodsPro3Test.kt
@@ -74,4 +74,21 @@ class AirPodsPro3Test : BaseAirPodsTest() {
             model shouldBe PodDevice.Model.AIRPODS_PRO3
         }
     }
+
+    @Test
+    fun `AirPods Pro 3`() = runTest {
+        create<AirPodsPro3>("07 19 01 27 20 0B 99 8F 11 00 05 43 A8 85 17 07 FF 41 62 2C FE 5E 20 08 1A 52 40") {
+            isLeftPodInEar shouldBe true
+            isRightPodInEar shouldBe true
+
+            isCaseCharging shouldBe false
+            isRightPodCharging shouldBe false
+            isLeftPodCharging shouldBe false
+            batteryCasePercent shouldBe null
+
+            podStyle.identifier shouldBe HasAppleColor.DeviceColor.WHITE.name
+
+            model shouldBe PodDevice.Model.AIRPODS_PRO3
+        }
+    }
 }
