699c90bf449764ccd2055f23d8dbc19d14650b0c
darken <darken@darken.eu>
2026-02-18 23:22:27 +0100

    chore: Bump Android Gradle Plugin to 9.0.1

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -18,7 +18,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.android.tools.build:gradle:9.0.0")
+    implementation("com.android.tools.build:gradle:9.0.1")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.2.10")
     implementation("com.squareup:javapoet:1.13.0")
 }
